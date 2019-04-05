ae829663bcd3c295a25357a697b925a7bbd20546
UnbiasedBrigade <48119401+UnbiasedBrigade@users.noreply.github.com>
2019-04-05 21:05:14 +0530

    basic HTML-JS selectors

---
+++ HTML-JS_Toolbox.js
@@ -0,0 +1,19 @@
+var queryEl = document.querySelector("QueryID"); //creates a variable 'queryEl' from a query of the HTML for 'QueryID'
+
+var queryEls = document.getElementsByClassName("ClassName"); //creates a variable 'queryEls' from a class of HTML 'ClassName'
+
+var idEl = document.getElementById("your-ID"); // creates a variable 'idEl' from an HTML id 'your-ID'
+
+var tagEls = document.getElementByTagName("p"); // creates a variable 'tagEls' from the HTML tag 'h1'
+
+queryEl.innerHTML = "Replace this for the query"; /*replaces queryEl's value with 'Replace this fore the query'if there is only one
+Element selected by the query */
+
+for (var i = 0; i < queryEl.length; i++) {
+  queryEls[i].innerHTML = "Replace this for the query"; /*Use this if there are multiple Elements in queryEl*/
+}
+
+console.log(queryEl); /*Prints querryEl to the console. on windows, you open it with 'ctrl-Shift-I' and then in the left side click
+'console' */
+
+document.body.innerHTML = "this is the new webpage" //replaces the whole webpage for 'this is the new webpage'
